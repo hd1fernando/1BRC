ee147cb1ed92ad9f84d79ae72f03687db48679c9
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2025-04-29 19:49:29 -0300

    initial commit: stupit implemetation, 47s to run 1M rows

---
+++ 1BRC.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿namespace _1BRC.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var station = new Station
+            {
+                Values = new List<float>
+                {
+                    10.0f, 20.0f, 30.0f
+                },
+                Name = "MyStation"
+            };
+
+            var min = station.Min();
+            var max = station.Max();
+            var mean = station.Mean();
+
+            Assert.Equal(10.0f,min);
+            Assert.Equal(30.0f,max);
+            Assert.Equal(20.0f, mean);
+        }
+    }
+}
---
+++ 1BRC/Station.cs
@@ -0,0 +1,12 @@
+﻿public class Station
+{
+    public string?  Name { get; set; }
+    public List<float> Values = new();
+    public float Mean() => Values.Average();
+
+    public float Min() => Values.Min();
+
+    public float Max() => Values.Max();
+
+    public override string ToString()=> $"{Name}={Min()}/{Mean()}/{Max()}";
+}
